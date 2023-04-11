74db687b323a5b57f41659aec125b65c791b0014
linder3hs <linderhassinger00@gmail.com>
2023-04-10 23:30:45 -0500

    ➕ feat: add ckeditor

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,19 @@
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+
 export default function Home() {
   return (
     <>
-      <h1>Home</h1>
+      <div className="p-10 bg-gray-200">
+        <div className="artboard-demo phone-2 p-6">
+          <h1 className="text-2xl">
+            Escribe un post y comparte tu conocimiento!
+          </h1>
+          <div className="mt-10">
+            <CKEditor editor={ClassicEditor} />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
