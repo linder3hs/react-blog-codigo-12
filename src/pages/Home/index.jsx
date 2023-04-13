5d2aebf059cc9abd9c1985ce767925e7c9b95cc5
linder3hs <linderhassinger00@gmail.com>
2023-04-12 22:08:27 -0500

    👨🏻‍💻 chore: add event

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,14 @@
-import { CKEditor } from "@ckeditor/ckeditor5-react";
+import { useState } from "react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
 
 export default function Home() {
+  const [content, setContent] = useState("");
+
+  const handleCreatePost = async () => {
+    console.log(content);
+  };
+
   return (
     <>
       <div className="p-10 bg-gray-200">
@@ -10,7 +17,15 @@ export default function Home() {
             Escribe un post y comparte tu conocimiento!
           </h1>
           <div className="mt-10">
-            <CKEditor editor={ClassicEditor} />
+            <CKEditor
+              editor={ClassicEditor}
+              onChange={(_, editor) => setContent(editor.getData())}
+            />
+          </div>
+          <div className="mt-10">
+            <button onClick={handleCreatePost} className="btn btn-primary">
+              Guardar
+            </button>
           </div>
         </div>
       </div>
